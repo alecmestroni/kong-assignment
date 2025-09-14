78c73ad7da20b258c7fcaa9cdef46bfbc647494a
alecjs <alecmestroni@gmail.com>
2025-09-14 19:38:01 +0200

    feat: add test reporting and CI upload-artifact
    - Add cypress reporting with cypress-multi-reporters and cypress-xray-junit-reporter
    - Update cypress config with detailed reporter options
    - Add GitHub Actions workflow for automated testing
    - Configure report merging and artifact uploads
    - Update .gitignore to exclude report directory
    - Bump version to 0.5.0

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     // sometimes we got cross-origin uncaught exception
+    defaultCommandTimeout: 10_000,
     retries: 1,
+    betterRetries: true,
+    deleteVideoOnPassed: true,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      reporterEnabled: "spec, cypress-xray-junit-reporter",
+      cypressXrayJunitReporterReporterOptions: {
+        mochaFile: "./report/[suiteFilename].xml",
+        useFullSuiteTitle: false,
+        jenkinsMode: true,
+        xrayMode: true,
+        attachScreenshot: true,
+        shortenLogMode: true,
+      },
+    },
     baseUrl: "http://localhost:8002",
     setupNodeEvents(on, config) {
       require("cypress-env")(on, config, __dirname);
